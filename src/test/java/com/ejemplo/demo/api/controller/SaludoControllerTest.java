8f8f212378ae587cb55395235fda294dc23b077b
Rafael <jpereza62@miumg.edu.gt>
2026-04-09 16:52:16 -0600

    Fix: [Captura y test]

--- src/test/java/com/ejemplo/demo/api/controller/SaludoControllerTest.java
+++ src/test/java/com/ejemplo/demo/api/controller/SaludoControllerTest.java
@@ -1,22 +1,31 @@
 package com.ejemplo.demo.api.controller;
 
+import com.ejemplo.demo.api.dto.SaludoResponse;
+import com.ejemplo.demo.domain.service.SaludoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.http.MediaType;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
 
 @WebMvcTest(SaludoController.class)
 class SaludoControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private SaludoService saludoService;
 
     @Test
     @DisplayName("Debe responder health del workshop")
@@ -28,11 +37,14 @@ class SaludoControllerTest {
 
     @Test
     @DisplayName("Prueba de saludo exitoso")
-    void pruebaSaludoExitoso() throws Exception{
-    	mockMvc.perform(get("/api/v1/saludos")
-    			.param("nombre","Ana"))
-    			.andExpect(status().isOk())
-    			.andExpect(jsonPath("$.mensaje").value("Hola Ana"));
+    void pruebaSaludoExitoso() throws Exception {
+        SaludoResponse mockResponse = new SaludoResponse("Hola Ana", Instant.now());
+        when(saludoService.crearSaludo("Ana")).thenReturn(mockResponse);
+
+        mockMvc.perform(get("/api/v1/saludos")
+                .param("nombre", "Ana"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.mensaje").value("Hola Ana"));
     }
     
     @Test
